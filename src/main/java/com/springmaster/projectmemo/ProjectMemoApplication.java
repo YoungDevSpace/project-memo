377fbd0d5ffa83b7c0a8064053b7f225fa0c0e45
YoungDevSpace <iamsoyoungdev@gmail.com>
2023-06-15 18:31:58 +0900

    init commit

---
+++ src/main/java/com/springmaster/projectmemo/ProjectMemoApplication.java
@@ -0,0 +1,13 @@
+package com.springmaster.projectmemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springmaster/projectmemo/ProjectMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmaster.projectmemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
